461675f7605061309bbd35c4908528603871b7ae
Raphael Santos Duarte <raphael-duarte@outlook.com>
2017-08-17 11:31:05 -0300

    Primeiro commit.

---
+++ TesteConsoleApp/TesteConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mensagem exibida na tela.
+            Console.WriteLine("Exemplo de uso do GitHub");
+
+            //aguardar uma tecla ser pressionada.
+            Console.ReadKey();
+        }
+    }
+}
